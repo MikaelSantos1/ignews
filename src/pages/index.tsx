010f50670f91d540a4bfde04a8b1f119f42cd30c
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-02-28 11:26:06 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+   <h1 className={styles.title}>Hello world!</h1>
+  )
+}
